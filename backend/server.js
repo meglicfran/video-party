71da6bf91609eda3978cee96933038b76946db79
Fran Meglic <meglic.fran@gmail.com>
2025-07-12 16:02:33 +0200

    server client communication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,33 @@
 console.log("Hello!")
+
+const WebSocket = require("ws");
+const { v4: uuidv4 } = require('uuid');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+var clients = []
+
+wss.on('connection', (ws) => {
+    ws.id = uuidv4();
+    clients.push(ws);
+    console.log('Client connected ' + ws.id);
+
+    // Receive message from client
+    ws.on('message', (rawData) => {
+        var message = rawData.toString();
+        var log = 'From ' + ws.id + ' Received:' + message;
+        console.log(log);
+
+        for (var client of clients) {
+            //if(client.id === ws.id) continue;
+            client.send(message);
+        }
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        clients = clients.filter((client) => {
+            return client.id !== ws.id
+        })
+    });
+});
